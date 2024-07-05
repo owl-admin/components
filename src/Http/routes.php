7a42357ae5cba1ccd0c3688813e5132ef70f773c
slowlyo <slowlyo_email@qq.com>
2024-07-05 21:35:37 +0800

    feat: 增加金额输入表单项

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -3,4 +3,4 @@
 use OwlAdmin\Components\Http\Controllers;
 use Illuminate\Support\Facades\Route;
 
-Route::get('components', [Controllers\ComponentsController::class, 'index']);
+// Route::get('components', [Controllers\ComponentsController::class, 'index']);
--- src/Traits/FormTrait.php
+++ src/Traits/FormTrait.php
@@ -21,4 +21,23 @@ trait FormTrait
             ->max(999999)
             ->description('越大越靠前');
     }
+
+    /**
+     * 金额输入
+     *
+     * @param $name
+     * @param $label
+     * @param $precision
+     *
+     * @return \Slowlyo\OwlAdmin\Renderers\NumberControl
+     */
+    public function formAmount($name, $label = '', $precision = 0.01)
+    {
+        return amis()->NumberControl($name, $label)
+            ->min(0.01)
+            ->max(99999999)
+            ->required()
+            ->step(0.01)
+            ->precision(0.01);
+    }
 }
